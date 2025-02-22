7751fcb9c3333175fe60be4d136ac6fd356b2aea
SUJAL RAJPUT <sujalrajput10@gmail.com>
2025-02-22 15:53:10 +0530

    removed token info

---
+++ src/config_example.py
@@ -0,0 +1,9 @@
+acc_token = "your acc token"
+agenta_token = "your agent token"
+
+BASE_URL = "https://api.spacetraders.io/v2"
+
+HEADERS = {
+    "Authorization": f"Bearer {agenta_token}",
+    "Content-Type": "application/json",
+}
